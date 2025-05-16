b78eaaf71d2b371358c3eba6eb73d2e81056fed0
kishan <kishan.shr01@gmail.com>
2025-05-16 20:52:02 +0545

    theme switcher

---
+++ themeswitcher/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode:"class", // note this configuration remember while adding darkmode theme
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
